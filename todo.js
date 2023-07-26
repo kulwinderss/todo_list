d26b3fb826b50b03ca9d8d9d7152ec2a56fe7858
kulwinder singh <kulwinders.kvbrgh@gmail.com>
2023-07-26 06:20:07 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -4,7 +4,6 @@ window.addEventListener('load', ()=>{
     const form= document.querySelector("#task-form");
     const input= document.querySelector("#task-input");
     const list= document.querySelector("#tasks");
-    const no_of_task = document.getElementById('counter-value');
 
 
     form.addEventListener('submit', (e)=>{
@@ -15,27 +14,34 @@ window.addEventListener('load', ()=>{
             return;
         } 
 
+ //counting no. of tasks
+ const countertask = document.getElementById('no_of_tasks');
+ count_task++;
+ countertask.innerHTML = count_task;
+
+        //..................task section..................
         const task_div = document.createElement("div");
         task_div.classList.add("task");
         list.appendChild(task_div);
        
-        //task section
+        //completed check button.................................
         const completed_button= document.createElement("button");
         completed_button.classList.add("Completed");
         completed_button.innerHTML = "O";
         task_div.appendChild(completed_button);
 
-        
+         //completed button event listener
+         completed_button.addEventListener('click', ()=>{
+            
+            task_input.style.textDecoration="line-through";
+            task_input.setAttribute("readonly", "readonly");
+           
+    })
+
+        //task text...............
         const task_content_div = document.createElement("div");
         task_content_div.classList.add("content");
         task_div.appendChild(task_content_div);
-
-        
-
-        //counting no. of tasks
-        const countertask = document.getElementById('no_of_tasks');
-        count_task++;
-        countertask.innerHTML = count_task;
         
 
         const task_input= document.createElement("input");
@@ -62,14 +68,9 @@ window.addEventListener('load', ()=>{
         delete_button.innerHTML = "Delete";
 
 
-        //create completed button
-       // const completed_button= document.createElement("button");
-      //  completed_button.classList.add("Completed");
-       // completed_button.innerHTML = "Completed";
-
         task_actions_div.appendChild(edit_botton);
-       // task_actions_div.appendChild(completed_button);
         task_actions_div.appendChild(delete_button);
+        
         
         //edit button event listener
         edit_botton.addEventListener('click', ()=>{
@@ -95,13 +96,7 @@ window.addEventListener('load', ()=>{
             }
         })
         
-     //completed button event listener
-        completed_button.addEventListener('click', ()=>{
-            
-                task_input.style.textDecoration="line-through";
-                task_input.setAttribute("readonly", "readonly");
-               
-        })
+    
 
         input.value = "";
 
